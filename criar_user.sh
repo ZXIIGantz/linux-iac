2781f0c2141c11bc3a5b2f4441f1a586d0999446
Caio Santos Araujo <79547691+ZXIIK-YO@users.noreply.github.com>
2023-10-06 12:19:33 -0300

    Update criar_user.sh

--- criar_user.sh
+++ criar_user.sh
@@ -2,13 +2,13 @@
 
 echo "CRIANDO USUÁRIOS DO SISTEMA..."
 
-useradd guest10 -c "usuário foda" -s /bin/bash -m -p $(openssl passwd senha123)
+useradd guest10 -c "usuário padrão" -s /bin/bash -m -p $(openssl passwd senha123)
 passwd guest10 -e
 
-useradd guest11 -c "usuário foda" -s /bin/bash -m -p $(openssl passwd senha123)
+useradd guest11 -c "usuário padrão" -s /bin/bash -m -p $(openssl passwd senha123)
 passwd guest11 -e
 
-useradd guest12 -c "usuário foda" -s /bin/bash -m -p $(openssl passwd senha123)
+useradd guest12 -c "usuário padrão" -s /bin/bash -m -p $(openssl passwd senha123)
 passwd guest12 -e
 
 echo "USUÁRIOS CRIADOS!"
